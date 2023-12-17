54c35cb3917ad9f1f2264a5a6a8fe0ab142d371b
Surya J <suryaj.cse2022@citchennai.net>
2023-12-17 13:29:59 +0530

    first

---
+++ lib/data/database.dart
@@ -0,0 +1,25 @@
+import 'package:hive_flutter/hive_flutter.dart';
+
+class ToDoDataBase {
+  List toDoList = [];
+
+  // reference our box
+  final _myBox = Hive.box('mybox');
+
+  // run this method if this is the 1st time ever opening this app
+  void createInitialData() {
+    toDoList = [
+      
+    ];
+  }
+
+  // load the data from database
+  void loadData() {
+    toDoList = _myBox.get("TODOLIST");
+  }
+
+  // update the database
+  void updateDataBase() {
+    _myBox.put("TODOLIST", toDoList);
+  }
+}
